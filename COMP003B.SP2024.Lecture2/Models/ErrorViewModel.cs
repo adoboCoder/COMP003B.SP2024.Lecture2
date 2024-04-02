361e2fb99fd8932d9cca4c2079111054df6cfdb2
adobocoder <jcruz@2cruz.com>
2024-04-01 18:31:29 -0700

    Add project files.

---
+++ COMP003B.SP2024.Lecture2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.SP2024.Lecture2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
